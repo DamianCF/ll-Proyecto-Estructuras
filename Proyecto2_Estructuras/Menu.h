b4ed8d2458c8cfb4f0faf7077014618bd3088191
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2021-11-11 15:11:52 -0600

    SFML
    Tuve fe

---
+++ Proyecto2_Estructuras/Menu.h
@@ -0,0 +1,41 @@
+#pragma once
+#pragma once
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+#define Max_menu 5
+
+class Menu {
+private:
+
+	int MenuSeleccionado;
+	int fps;//cantidad de frames por segundo en que se actualiza la pantalla
+
+	RenderWindow* menuPrincipal;//ventada que almacena el menu principal de la aplicacion
+	Texture* texturaFondo;
+	Sprite* basePantalla;
+	Event* evento;
+
+	Font* fuente;
+	Text vectorBotones[Max_menu];
+
+	SoundBuffer buffer;
+	Sound click;
+public:
+	Menu(int ancho, int alto, string titulo);
+	~Menu();
+	void gameloop();
+	void dibujar();
+	void ejecutarEventos();
+	void MoverArriba();
+	void Moverabajo();
+
+	int MenuPressed()
+	{
+		return MenuSeleccionado;
+	}
+};
---
+++ Proyecto2_Estructuras/main.cpp
@@ -0,0 +1,17 @@
+#include "SFML/Graphics.hpp"
+#include "iostream"
+#include "Menu.h"
+#include <cstdlib>
+#include <string>
+
+using namespace sf;
+using namespace std;
+
+int main(int argc, char* args[])
+{
+
+	Menu* partida;
+	partida = new Menu(960, 540, "SOKOBAN");
+
+	return 0;
+}
